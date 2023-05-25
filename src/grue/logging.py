9d91639ab36b0feb3281b08a01b13639508a9190
Jeff Nyman <jeffnyman@gmail.com>
2023-05-25 09:05:31 -0500

    Updates for quality checks.

--- src/grue/logging.py
+++ src/grue/logging.py
@@ -1,12 +1,18 @@
+"""Logging module for Grue."""
+
 import os
 
 
 def setup_logging(filename: str) -> None:
+    """Set up a new log by truncating any existing log file."""
+
     if os.path.exists(filename):
         with open(filename, "w"):
             pass
 
 
 def log(message: str) -> None:
+    """Write a log statement to an existing log file."""
+
     with open("log.txt", "a") as logfile:
         print(message, file=logfile)
