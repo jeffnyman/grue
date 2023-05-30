d1392faf0499465379a878564314adf781256213
Jeff Nyman <jeffnyman@gmail.com>
2023-05-30 08:50:44 -0500

    Adding store identifier to opcodes.

--- src/grue/opcodes.py
+++ src/grue/opcodes.py
@@ -7,10 +7,13 @@ from dataclasses import dataclass
 class Opcodes:
     """Represents data values to identify unique opcodes."""
 
-    def __init__(self, name: str, versions: list, value: tuple) -> None:
+    def __init__(
+        self, name: str, versions: list, value: tuple, store: bool = False
+    ) -> None:
         self.name = name
         self.versions = versions
         self.value = value
+        self.store = store
 
     def matches(self, version: int, byte: int, number: int) -> bool:
         """Checks for matching Opcode data class based on parameters."""
@@ -19,6 +22,6 @@ class Opcodes:
 
 
 opcodes = [
-    Opcodes(name="call", versions=[1, 2, 3], value=(224, 0)),
-    Opcodes(name="call_vs", versions=[5, 6, 7, 8], value=(224, 0)),
+    Opcodes(name="call", versions=[1, 2, 3], value=(224, 0), store=True),
+    Opcodes(name="call_vs", versions=[5, 6, 7, 8], value=(224, 0), store=True),
 ]
