000938d3890bd7628b6e1486474ca173b417da81
Jeff Nyman <jeffnyman@gmail.com>
2023-07-11 15:53:40 -0500

    Handling arguments passed to package.

--- src/grue/__main__.py
+++ src/grue/__main__.py
@@ -1,10 +1,14 @@
 import sys
+from typing import Optional
 
 from grue import __version__
 
 
-def main() -> int:
+def main(args: Optional[list] = None) -> int:
     print(f"\nGrue Z-Machine Interpreter (Version: {__version__})\n")
+
+    if not args:
+        args = sys.argv[1:]
 
     return 0
 
