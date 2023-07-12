cfc8c33eb6061a032bdbe8dfeca8636fba7222c4
Jeff Nyman <jeffnyman@gmail.com>
2023-07-12 06:05:00 -0500

    Added expectations format for tests.

--- tests/test_grue.py
+++ tests/test_grue.py
@@ -1,9 +1,12 @@
+from expects import expect, equal, contain
+
+
 def test_package_version() -> None:
     """Current version is exposed on the package."""
 
     from grue import __version__
 
-    assert __version__ == "0.1.0"
+    expect(__version__).to(equal("0.1.0"))
 
 
 def test_grue_startup_banner(capsys) -> None:
@@ -17,4 +20,5 @@ def test_grue_startup_banner(capsys) -> None:
     captured = capsys.readouterr()
     result = captured.out
 
-    assert f"Grue Z-Machine Interpreter (Version: {__version__})" in result
+    banner_text = f"Grue Z-Machine Interpreter (Version: {__version__})"
+    expect(result).to(contain(banner_text))
