6da1aea83310e14d909da12c6e83c3f0fcaa38fc
Jeff Nyman <jeffnyman@gmail.com>
2023-07-12 06:46:50 -0500

    Added a check for minimal Python version.

--- src/grue/startup.py
+++ src/grue/startup.py
@@ -7,7 +7,20 @@ from grue import __version__
 def main(args: Optional[list] = None) -> int:
     print(f"\nGrue Z-Machine Interpreter (Version: {__version__})\n")
 
+    check_python_version()
+
     if not args:
         args = sys.argv[1:]
 
     return 0
+
+
+def check_python_version() -> None:
+    python_version = (
+        f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
+    )
+
+    if sys.version_info < (3, 8, 2):
+        sys.stderr.write("\nGrue requires Python 3.8.2 or later.\n")
+        sys.stderr.write(f"Your current version is {python_version}\n\n")
+        sys.exit(1)
--- tests/test_grue.py
+++ tests/test_grue.py
@@ -1,3 +1,4 @@
+import pytest
 from expects import expect, equal, contain
 
 
@@ -22,3 +23,21 @@ def test_grue_startup_banner(capsys) -> None:
 
     banner_text = f"Grue Z-Machine Interpreter (Version: {__version__})"
     expect(result).to(contain(banner_text))
+
+
+def test_bad_python_version(monkeypatch, capsys) -> None:
+    """Checks if Python version requirement is met."""
+
+    import sys
+    from grue.startup import check_python_version
+
+    monkeypatch.setattr(sys, "version_info", (3, 7, 9))
+
+    with pytest.raises(SystemExit):
+        check_python_version()
+
+    captured = capsys.readouterr()
+    result = captured.err
+
+    error_text = "Grue requires Python 3.8.2 or later."
+    expect(result).to(contain(error_text))
