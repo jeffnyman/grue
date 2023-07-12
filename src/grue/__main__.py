7fa7e4f55aa9e15abfb26bffc7c725e81e40612d
Jeff Nyman <jeffnyman@gmail.com>
2023-07-12 06:37:58 -0500

    Refactor to include module for app startup.

--- src/grue/__main__.py
+++ src/grue/__main__.py
@@ -1,17 +1,6 @@
 import sys
-from typing import Optional
 
-from grue import __version__
-
-
-def main(args: Optional[list] = None) -> int:
-    print(f"\nGrue Z-Machine Interpreter (Version: {__version__})\n")
-
-    if not args:
-        args = sys.argv[1:]
-
-    return 0
-
+from grue.startup import main
 
 if __name__ == "__main__":
     sys.exit(main())
---
+++ src/grue/startup.py
@@ -0,0 +1,13 @@
+import sys
+from typing import Optional
+
+from grue import __version__
+
+
+def main(args: Optional[list] = None) -> int:
+    print(f"\nGrue Z-Machine Interpreter (Version: {__version__})\n")
+
+    if not args:
+        args = sys.argv[1:]
+
+    return 0
