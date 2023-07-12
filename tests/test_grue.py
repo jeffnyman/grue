2438592ffa4a0e94df40ad70f8be7098274f6add
Jeff Nyman <jeffnyman@gmail.com>
2023-07-12 03:31:20 -0500

    Added a specification based test output.

--- tests/test_grue.py
+++ tests/test_grue.py
@@ -1,10 +1,14 @@
 def test_package_version() -> None:
+    """Current version is exposed on the package."""
+
     from grue import __version__
 
     assert __version__ == "0.1.0"
 
 
 def test_grue_startup_banner(capsys) -> None:
+    """Provides a minimal banner on startup."""
+
     from grue.__main__ import main
     from grue import __version__
 
